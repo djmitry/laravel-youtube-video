8b6c058fcc4fd9a1338b5ef9d8751a32c1d13eb6
Dmitry <d.i.ma.92@mail.ru>
2019-05-02 19:55:01 +0600

    directive params

--- src/YoutubeServiceProvider.php
+++ src/YoutubeServiceProvider.php
@@ -9,8 +9,12 @@ class YoutubeServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        Blade::directive('youtube', function($expression) {
-            return '<iframe width="560" height="315" src="' . $expression . '" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+        Blade::directive('youtube', function($params) {
+            $params = explode(', ', $params);
+            $src = $params[0];
+            $w = !empty($params[1]) ? $params[1] : 560;
+            $h = !empty($params[2]) ? $params[2] : 312;
+            return '<iframe width="' . $w . '" height="' . $h . '" src="https://www.youtube.com/embed/' . $src . '" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
         });
     }   
 }
